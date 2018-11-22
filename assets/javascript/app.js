298951a108911020285dd9ef0606d5c7453d7814
Jessica Nguyen <jessicanguyen320@gmail.com>
2018-11-21 19:22:09 -0800

    changed topics array to contain objects with key value pairs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,23 @@ var API_KEY = 'qmLNsA8nFrXwkZ1du7Y6SIDwklV37AaD';
 
 // Topics array
 var topics = [
-    'Rhinos',
-    'Whales',
-    'Sharks'
+    {
+        search: 'Rhinos',
+        url: 'rhinos.com'
+    },
+    {
+        search: 'Whales',
+        url: 'whales.com'
+    },
+    {
+        search: 'Sharks',
+        url: 'sharks.com'
+    }
 ];
 
 // Loop through each array item and create an image element
 
 for (var i = 0; i < topics.length; i++) {
-    $('.giphy-buttons').append('<button>' + topics[i])
+    $('.giphy-buttons').append('<button>' + topics[i].search)
 
 }
